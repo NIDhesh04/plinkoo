350a89251f1e853f12bcd07bdd36850a64e56f4d
Nidhesh Soni <126197115+NIDhesh04@users.noreply.github.com>
2025-06-29 13:43:18 +0000

    fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,56 +1,56 @@
-
-import express from "express";
+import express, { Request, Response } from "express";
 import { outcomes } from "./outcomes";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 const TOTAL_DROPS = 16;
 
+const MULTIPLIERS: { [key: number]: number } = {
+  0: 16,
+  1: 9,
+  2: 2,
+  3: 1.4,
+  4: 1.4,
+  5: 1.2,
+  6: 1.1,
+  7: 1,
+  8: 0.5,
+  9: 1,
+  10: 1.1,
+  11: 1.2,
+  12: 1.4,
+  13: 1.4,
+  14: 2,
+  15: 9,
+  16: 16,
+};
 
+app.post("/game", (req: Request, res: Response) => {
+  let outcome = 0;
+  const pattern = [];
 
-
-const MULTIPLIERS: {[ key: number ]: number} = {
-    0: 16,
-    1: 9,
-    2: 2,
-    3: 1.4,
-    4: 1.4,
-    5: 1.2,
-    6: 1.1,
-    7: 1,
-    8: 0.5,
-    9: 1,
-    10: 1.1,
-    11: 1.2,
-    12: 1.4,
-    13: 1.4,
-    14: 2,
-    15: 9,
-    16: 16
-}
-
-app.post("/game", (req, res) => {
-    let outcome = 0;
-    const pattern = []
-    for (let i = 0; i < TOTAL_DROPS; i++) {
-        if (Math.random() > 0.5) {
-            pattern.push("R")
-            outcome++;
-        } else {
-            pattern.push("L")
-        }
+  for (let i = 0; i < TOTAL_DROPS; i++) {
+    if (Math.random() > 0.5) {
+      pattern.push("R");
+      outcome++;
+    } else {
+      pattern.push("L");
     }
+  }
 
-    const multiplier = MULTIPLIERS[outcome];
-    const possiblieOutcomes = outcomes[outcome];
+  const multiplier = MULTIPLIERS[outcome];
+  const possibleOutcomes = outcomes[outcome];
 
-    res.send({
-        point: possiblieOutcomes[Math.floor(Math.random() * possiblieOutcomes.length || 0)],
-        multiplier,
-        pattern
-    });
+  res.send({
+    point:
+      possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length || 0)],
+    multiplier,
+    pattern,
+  });
 });
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("✅ Backend server running on port 3000");
+});
